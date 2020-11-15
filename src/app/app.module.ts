e92e0f5528c06bbbea3b624840a3116ae3f99c8c
smujezinovic <smujezinovic@capeannenterprises.com>
2020-11-15 17:01:34 +0100

    Initial setup
    -Add Bootstrap
    -Add Router Module
    -Add Navbar and Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { LoginComponent } from './login/login.component';
+
+
+const routes: Routes = [
+  { path: 'shopping-cart', component: ShoppingCartComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingCartComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
